e8e35405412a56391fd03bc16fa19eb44aed2f1a
rishabh <rishab.bajpai@impressico.com>
2020-01-15 20:17:26 +0530

    dynamic stack

--- src/main/java/stacks/DynamicSizeStack.java
+++ src/main/java/stacks/DynamicSizeStack.java
@@ -3,21 +3,33 @@ public class DynamicSizeStack<E> implements IStack<E> {
     private float loadFactor=0.75f;
     private int threshold;
     private int top=-1;
+    private int size;
 
     @SuppressWarnings("unchecked")
     DynamicSizeStack(int initialSize)
     {
         array=(E[])new Object[initialSize];
-        threshold= (int) (initialSize*loadFactor);
+        size=initialSize;
+        threshold= (int) (size*loadFactor);
     }
 
     @Override
     public void push(E element) throws Exception {
             if(isFull())
             {
-                throw new Exception("Stack overflow Exception");
+               resize();
             }
             array[++top]=element;
+    }
+
+    @SuppressWarnings("unchecked")
+    private void resize() {
+        // double the size
+        size=size*2;
+        E[] newArray = (E[]) new Object[size];
+        System.arraycopy(array, 0, newArray, 0, array.length);
+        array=newArray;
+        threshold=(int) (size*loadFactor);
     }
 
     @Override
@@ -33,11 +45,18 @@ public class DynamicSizeStack<E> implements IStack<E> {
 
     @Override
     public boolean isEmpty() {
+        if (top == -1) {
+            return true;
+        }
         return false;
     }
 
     @Override
     public boolean isFull() {
+
+        if (top == size-1) {
+            return true;
+        }
         return false;
     }
 }
--- src/main/java/stacks/StackTest.java
+++ src/main/java/stacks/StackTest.java
@@ -3,7 +3,7 @@ import java.util.Stack;
 public class StackTest {
 
     public static void main(String[] args) throws Exception {
-        DynamicSizeStack<Integer> stack= new DynamicSizeStack<>(5);
+        DynamicSizeStack<Integer> stack= new DynamicSizeStack<>(2);
         stack.push(2);
         stack.push(6);
         stack.push(7);
