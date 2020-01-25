a1e4f4a3a25592429a409a48b407e626a72545eb
Rishabh <rishabh.bajpai@impressico.com>
2020-01-25 18:04:34 +0530

    TOH

--- src/main/java/stacks/TowerOfHanoi.java
+++ src/main/java/stacks/TowerOfHanoi.java
@@ -9,19 +9,19 @@ import java.util.Stack;
 public class TowerOfHanoi {
     public static void main(String[] args) {
         Stack<Integer> stack=new Stack<>();
-        towerOfHanoi(4,'A','B','C');
+        towerOfHanoi(3,'A','B','C');
     }
 
     private static void towerOfHanoi(int n, char a, char b, char c) {
 
         if(n==1)
         {
-            System.out.println(a+"->"+b);
+            System.out.println(a+"->"+c);
             return;        }
 
         towerOfHanoi(n-1,a,c,b);// head recursion
-        System.out.println(a+"->"+b);
-        towerOfHanoi(n - 1, c, b, a);// tail recursion
+        System.out.println(a+"->"+c);
+        towerOfHanoi(n - 1, b, a, c);// tail recursion
 
 
     }
