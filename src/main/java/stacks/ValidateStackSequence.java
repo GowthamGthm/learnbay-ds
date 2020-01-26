3675db0dcc289b3e835911ca9982e33eff04c382
Rishabh <rishabh.bajpai@impressico.com>
2020-01-27 01:29:44 +0530

    validate-sequence-problem

---
+++ src/main/java/stacks/ValidateStackSequence.java
@@ -0,0 +1,31 @@
+import java.util.Stack;
+
+public class ValidateStackSequence {
+
+        public static boolean validateStackSequences(int[] pushed, int[] popped) {
+
+            Stack<Integer> stack=new Stack<>();
+            int j=0;
+            for(int i=0;i<pushed.length;i++)
+            {
+                stack.push(pushed[i]);
+                while(!stack.isEmpty() && stack.peek()==popped[j])
+                {
+                    stack.pop();
+                    j++;
+
+                }
+
+            }
+
+
+            return j==pushed.length;
+        }
+
+    public static void main(String[] args) {
+        int[] pushed=new int[]{1,2,3,4,5};
+        int[] popped=new int[]{4,5,3,2,1};
+
+        System.out.println(validateStackSequences(pushed,popped));
+    }
+}
