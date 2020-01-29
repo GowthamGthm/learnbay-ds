e0f8fc0779a7feca36d7a3be65328354ec1bdceb
rishabh <rishab.bajpai@impressico.com>
2020-01-29 13:01:28 +0530

    trap-rain-water

--- src/main/java/stacks/rainWaterTrapProblem.java
+++ src/main/java/stacks/rainWaterTrapProblem.java
@@ -1,3 +1,5 @@
+import java.util.Stack;
+
 public class rainWaterTrapProblem {
 
 
@@ -36,9 +38,37 @@ public class rainWaterTrapProblem {
   }
 
 
+  static int trapWaterUsingStack(int[] height)
+  {
+
+    int unitOfWaterTrapped=0;
+
+    Stack<Integer> stack =new Stack<>();
+    int n=height.length;
+    for(int i=0;i<n;i++)
+    {
+
+      while(!stack.isEmpty() && height[i]>height[stack.peek()])
+      {
+        int top=stack.pop();
+        if(!stack.isEmpty())
+        {
+          int lMax= height[stack.peek()];
+          int rMax=height[i];
+          unitOfWaterTrapped+=(Math.min(lMax,rMax)-height[top])*(i-stack.peek()-1);
+        }
+
+
+      }
+      stack.push(i);
+
+    }
+    return unitOfWaterTrapped;
+  }
 
   public static void main(String[] args) {
     System.out.println(trapWaterSolutionOne(new int[]{0,1,0,2,1,0,1,3,2,1,2,1}));
+    System.out.println(trapWaterUsingStack(new int[]{0,1,0,2,1,0,1,3,2,1,2,1}));
 
   }
 }
