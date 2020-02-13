7b1e7973ef05ef0fe693e231bd5ea2c4948a959b
Rishabh <rishabh.bajpai@impressico.com>
2020-02-14 02:02:04 +0530

    queue-reverse-using-stack

--- src/main/java/Queues/ReverseQueueUsingRecursion.java
+++ src/main/java/Queues/ReverseQueueUsingRecursion.java
@@ -2,7 +2,7 @@
 import java.util.LinkedList;
 import java.util.Queue;
 
-public class ReverseQueue {
+public class ReverseQueueUsingRecursion {
     public static void main(String[] args) {
         Queue<Integer> queue=new LinkedList<Integer>();
         queue.add(1);
